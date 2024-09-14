a05fea1409991ff21d8a9ab9aa2d8ba9c3f927d0
DevRamona <r.ingabire@alustudent.com>
2024-09-14 22:45:58 +0200

    updated changes

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
-const{}
-const readData = ()=>{
+const{v4:uuid4} = require("uuid")
+const mongoose = require("mongoose")
+
+app.use(express.json())
+const readData = () => {
   try {
-    const data = fs.readFileSync("data.json", "utf-8")
+    const data = fs.readFileSync("data.json", "utf-8");
+
     return JSON.parse(data)
-  }catch(error) {
-    console.log(error.message)
+  } catch (error) {
+    console.log(error.message);
   }
+};
+
+const writeData = (data) => {
+ fs.writeFileSync("data.json", JSON.stringify(data, null, 2))
 }
 
-app.get("/", (request, response)=>{
-  const items = readData()
-  response.status(200).json(items)
-})
+app.get("/", (request, response) => {
+  const items = readData();
+  response.status(200).json(items);
+});
 
+app.post("/", (request,response) => {
+  const items = readData()
+
+  const newItems = {id:uuid4(), ...request.body}
+  items.push(newItems)
+  writeData(items)
+  response.status(201).json(newItems)
+
+})
 const port = 3000;
 app.listen(port, () => {
   console.log("CRUD Exercise");
