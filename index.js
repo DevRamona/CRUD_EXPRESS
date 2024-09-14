40fc37d081857bcec71eac13ab8c80b348c85711
DevRamona <r.ingabire@alustudent.com>
2024-09-14 10:31:30 +0200

    install uuid

--- index.js
+++ index.js
@@ -1,78 +1,20 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
-app.use(express.json());
-
-let data;
-try {
-  data = JSON.parse(fs.readFileSync(`${__dirname}/data.json`));
-  console.log("The required data", data);
-} catch (error) {
-  console.log("Error fetched", error.message);
-}
-app.get("/info", (request, response) => {
-  response.status(200).json({
-    data,
-  });
-});
-
-app.post("/info", (request, response) => {
-  const newId = data[data.length - 1].id + 1;
-  const newData = Object.assign({ id: newId }, request.body);
-  data.push(newData);
-  fs.writeFile(`${__dirname}/data.json`, JSON.stringify(data), (err) => {
-    response.status(200).json({
-      status: "success",
-      data: {
-        newData,
-      },
-    });
-  });
-});
-app.get("info/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const datas = data.filter((element) => element.id === id);
-  if (datas.length === 0) {
-    return response.status(404).json({
-      status: "fail",
-    });
+const{}
+const readData = ()=>{
+  try {
+    const data = fs.readFileSync("data.json", "utf-8")
+    return JSON.parse(data)
+  }catch(error) {
+    console.log(error.message)
   }
-  response.status(200).json({
-    status: "success",
-    data: {
-      datas,
-    },
-  });
-});
-app.get("info/:id", (request, response) => {
+}
 
-    if (request.params.id > data.length) {
-      return response.status(404).json({
-        status: "fail",
-      });
-    }
-    response.status(200).json({
-      status: "success",
-      data: {
-      data
-      },
-    });
-  });
-
-  app.delete("info/:id", (request, response) => {
-
-    if (request.params.id > data.length) {
-      return response.status(404).json({
-        status: "fail",
-      });
-    }
-    response.status(200).json({
-      status: "success",
-      data: null,
-    });
-  });
-
-
+app.get("/", (request, response)=>{
+  const items = readData()
+  response.status(200).json(items)
+})
 
 const port = 3000;
 app.listen(port, () => {
